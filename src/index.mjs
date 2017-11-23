a8206f01d0164124f8aa18be50ed34b56ee1e0d1
Michel Ribeiro <michel.ribeiro1@hotmail.com>
2017-11-23 08:52:36 -0300

    Remove async and await for then

--- src/index.mjs
+++ src/index.mjs
@@ -1,79 +1,101 @@
-import { GraphQLScalarType } from 'graphql'
-import Busboy from 'busboy'
-import objectPath from 'object-path'
+'use strict'
 
-export const GraphQLUpload = new GraphQLScalarType({
+Object.defineProperty(exports, '__esModule', {
+  value: true
+})
+exports.apolloUploadExpress = exports.apolloUploadKoa = exports.processRequest = exports.GraphQLUpload = void 0
+
+var _graphql = require('graphql')
+
+var _busboy = _interopRequireDefault(require('busboy'))
+
+var _objectPath = _interopRequireDefault(require('object-path'))
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
+
+const GraphQLUpload = new _graphql.GraphQLScalarType({
   name: 'Upload',
   description:
     'The `Upload` scalar type represents a file upload promise that resolves ' +
     'an object containing `stream`, `filename`, `mimetype` and `encoding`.',
   parseValue: value => value,
+
   parseLiteral() {
     throw new Error('Upload scalar literal unsupported')
   },
+
   serialize() {
     throw new Error('Upload scalar serialization unsupported')
   }
 })
+exports.GraphQLUpload = GraphQLUpload
 
-export const processRequest = (
+const processRequest = (
   request,
   { maxFieldSize, maxFileSize, maxFiles } = {}
 ) =>
   new Promise(resolve => {
-    const busboy = new Busboy({
+    const busboy = new _busboy.default({
       headers: request.headers,
       limits: {
         fieldSize: maxFieldSize,
-        fields: 2, // Only operations and map
+        fields: 2,
         fileSize: maxFileSize,
         files: maxFiles
       }
     })
-
-    // GraphQL multipart request spec:
-    // https://github.com/jaydenseric/graphql-multipart-request-spec
-
     let operations
     let operationsPath
-
     busboy.on('field', (fieldName, value) => {
       switch (fieldName) {
         case 'operations':
           operations = JSON.parse(value)
-          operationsPath = objectPath(operations)
+          operationsPath = (0, _objectPath.default)(operations)
           break
+
         case 'map': {
           for (const [mapFieldName, paths] of Object.entries(
             JSON.parse(value)
           )) {
-            // Upload scalar
             const upload = new Promise(resolve =>
               busboy.on(
                 'file',
                 (fieldName, stream, filename, encoding, mimetype) =>
                   fieldName === mapFieldName &&
-                  resolve({ stream, filename, mimetype, encoding })
+                  resolve({
+                    stream,
+                    filename,
+                    mimetype,
+                    encoding
+                  })
               )
             )
 
             for (const path of paths) operationsPath.set(path, upload)
           }
+
           resolve(operations)
         }
       }
     })
-
     request.pipe(busboy)
   })
 
-export const apolloUploadKoa = options => async (ctx, next) => {
+exports.processRequest = processRequest
+
+const apolloUploadKoa = options => (ctx, next) => {
   if (ctx.request.is('multipart/form-data'))
-    ctx.request.body = await processRequest(ctx.req, options)
-  await next()
+    processRequest(ctx.req, options).then((r) => {
+      ctx.request.body = r;
+      next()
+    })
 }
 
-export const apolloUploadExpress = options => (request, response, next) => {
+exports.apolloUploadKoa = apolloUploadKoa
+
+const apolloUploadExpress = options => (request, response, next) => {
   if (!request.is('multipart/form-data')) return next()
   processRequest(request, options)
     .then(body => {
@@ -82,3 +104,5 @@ export const apolloUploadExpress = options => (request, response, next) => {
     })
     .catch(next)
 }
+
+exports.apolloUploadExpress = apolloUploadExpress
